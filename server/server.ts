3f1323b0f260a325d1d5979d712f4497da501f45
Dmytro Kolosovskyi <kolosovskyi.dmytro@gmail.com>
2023-11-16 17:58:41 +0100

    Added logger

--- server/server.ts
+++ server/server.ts
@@ -1,8 +1,32 @@
 import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import path from "path";
+
+import { logger, logEvents } from "./middleware/logger";
+import errorHandler from "./middleware/errorHandler";
+import db from "./models";
 
 const app = express();
 
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["POST", "GET", "OPTIONS", "PUT"],
+    credentials: true,
+    optionsSuccessStatus: 200,
+  })
+);
 
-app.listen(3333, () => {
-    console.log("Server is running onn port 3333");
+app.use(logger);
+app.use(errorHandler);
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
+
+const PORT = process.env.PORT || 3333;
+console.log("E-commerce Server");
+
+db.sequelize.sync().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running onn port ${PORT}`);
+  });
 });
