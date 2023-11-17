2287f992364b6924c935acb0617b02f4bbd6fd14
Dmytro Kolosovskyi <kolosovskyi.dmytro@gmail.com>
2023-11-18 00:01:07 +0100

    Header restyle

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,19 +1,31 @@
 import { Link, NavLink } from "react-router-dom";
 import { FaShoppingCart } from "react-icons/fa";
+import logo from "../assets/logo.png";
 
 const Navbar = () => {
   return (
-    <div className="flex justify-between items-center p-2 md:mx-6 text-md sticky">
-      <NavLink to="/" className="font-semibold text-lg">
+    <div className="flex justify-between items-center px-5 mx-0 bg-white text-md sticky shadow-sm w-full z-100 ">
+      <NavLink to="/" className="font-semibold text-lg flex gap-1 items-center">
+        <img src={logo} alt="" width={60} height={60}/>  
         DK Shop
       </NavLink>
       <div className="flex gap-3 items-center">
         <NavLink to="/about" className="text-neutral-500 hover:text-black">
           About
         </NavLink>
-        <button type="button" className="w-10 h-10 rounded-full text-neutral-500 hover:text-black  p-2 relative">
-            <FaShoppingCart className="w-6 h-6"/>
-            <div className="rounded-full w-4 h-4 absolute bottom-0 right-1 text-white bg-red-500 flex justify-center items-center">3</div>
+        <NavLink to="/categories" className="text-neutral-500 hover:text-black">
+          Categories
+        </NavLink>
+      </div>
+      <div className="flex gap-3 items-center">
+        <button
+          type="button"
+          className="w-10 h-10 rounded-full text-neutral-500 hover:text-black  p-2 relative"
+        >
+          <FaShoppingCart className="w-6 h-6" />
+          <div className="rounded-full w-4 h-4 absolute bottom-0 right-1 text-white bg-red-500 flex justify-center items-center">
+            3
+          </div>
         </button>
       </div>
     </div>
