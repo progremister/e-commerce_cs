a5302a30ba8750da026e2c7df4965cbb03ab04bd
Dmytro Kolosovskyi <kolosovskyi.dmytro@gmail.com>
2023-11-17 16:55:29 +0100

    Added basic routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,14 @@
-import { useState } from 'react'
-import './App.css'
+import { Routes, Route } from "react-router-dom"
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <div className="px-15">
+      <Routes> 
+        <Route path="/" element={<Home />}/>
+        <Route path="/" element={<About />}/>
+      </Routes>
+    </div>
   )
 }
 
